990e6f61aa1c21c1c15d1f797098bce1e11bcdb6
Priyanka More <priyankmore1620@gmail.com>
2026-04-29 12:30:32 -0400

    fix product schema missing category field

--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-// sanity/schemaTypes.ts
-import post from "./post"
-
-// Sanity Studio expects 'schema' as the key
-export const schema = {
-  types: [post],
-}
--- types/product.ts
+++ types/product.ts
@@ -1,6 +1,10 @@
 export type Product = {
   id: number
   name: string
-  description?: string
+  description: string
   price: number
+  image: string
+  category: string
+  stock?: number
+  rating?: number
 }
